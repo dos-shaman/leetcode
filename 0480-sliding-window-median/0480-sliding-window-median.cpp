9fbcc1f95fd98dfbb06d0b072d6f71e7483a58b0
Ｍｏｈａｎ　Ｄａｓｓ <133029595+dos-shaman@users.noreply.github.com>
2024-07-05 11:45:07 +0530

    Time: 80 ms (98.86%), Space: 42.9 MB (49.77%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.cpp
+++ 0480-sliding-window-median/0480-sliding-window-median.cpp
@@ -1,50 +1,28 @@
-void partialSort(vector<int>& v, int pos) {
-    int val = v[pos];
-    auto pos_it = v.begin() + pos;
-
-    auto it = std::upper_bound(v.begin(), pos_it, val);
-    if (it != pos_it) {
-        std::copy_backward(it, pos_it, pos_it + 1);
-        *it = val;
-        return;
-    }
-
-    it = std::lower_bound(pos_it, v.end(), val);
-    if (it != pos_it) {
-        std::copy(pos_it + 1, it, pos_it);
-        *(--it) = val;
-    }
-}
-
+#pragma GCC optimize("O3", "unroll-loops")
+auto init = []() { 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
 class Solution {
 public:
     vector<double> medianSlidingWindow(vector<int>& nums, int k) {
-        if (nums.size() < k) {
-            return {};
+        vector<double> res;
+        multiset<int> window(nums.begin(), nums.begin() + k);
+        auto mid = next(window.begin(), k / 2);
+
+        for (int i = k; ; ++i) {
+            res.push_back(((double(*mid) + *prev(mid, 1 - k % 2)) / 2.0));
+
+            if (i == nums.size()) break;
+
+            window.insert(nums[i]);
+            if (nums[i] < *mid) --mid;
+            if (nums[i - k] <= *mid) ++mid;
+            window.erase(window.lower_bound(nums[i - k]));
         }
-
-        int mid1, mid2;
-        if (k % 2 == 0) {
-            mid1 = k / 2 - 1;
-            mid2 = k / 2;
-        } else {
-            mid2 = mid1 = k / 2;
-        }
-
-        vector<double> result;
-        result.reserve(nums.size() - k + 1);
-        vector<int> tmp;
-        tmp.insert(tmp.end(), nums.begin(), nums.begin() + k);
-        std::sort(tmp.begin(), tmp.end());
-
-        for (int i = 0; i < nums.size() - k; ++i) {
-            result.push_back(((double)tmp[mid1] + (double)tmp[mid2]) / 2.0);
-            auto it = std::lower_bound(tmp.begin(), tmp.end(), nums[i]);
-            *it = nums[i + k];
-            partialSort(tmp, std::distance(tmp.begin(), it));
-        }
-        result.push_back(((double)tmp[mid1] + (double)tmp[mid2]) / 2.0);
-
-        return result;
+        
+        return res;
     }
 };
