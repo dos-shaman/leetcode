1d41ebcda475a2d758a11d7631301f1b569caa39
Ｍｏｈａｎ　Ｄａｓｓ <133029595+dos-shaman@users.noreply.github.com>
2024-07-05 11:42:12 +0530

    Time: 80 ms (98.86%), Space: 39.3 MB (86.34%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.cpp
+++ 0480-sliding-window-median/0480-sliding-window-median.cpp
@@ -1,79 +1,89 @@
+#define p pair<double, int>
 class Solution {
 public:
-    multiset<long long> lHeap, rHeap;
-
-    void balance() 
-    {
-        if (lHeap.size() > rHeap.size() + 1) 
-        {
-            rHeap.insert(*lHeap.rbegin());
-            lHeap.erase(prev(lHeap.end()));
-        } 
-        else if (rHeap.size() > lHeap.size()) 
-        {
-            lHeap.insert(*rHeap.begin());
-            rHeap.erase(rHeap.begin());
+    priority_queue<p> leftMax;
+    priority_queue<p, vector<p>, greater<p>> rightMin;
+    int l = 0, r = 0;
+    vector<double> ans;
+    bool balance() {
+        bool flag = true;
+        // left == right (no. of element) or left can have 1 more
+        // if right have more than left
+        if (l == r or l == r + 1)
+            return true;
+        else if (l < r) {
+            flag = false;
+            leftMax.push(rightMin.top());
+            rightMin.pop();
+            l++;
+            r--;
         }
+        // if left have 2 and or more elements than right
+        else if (l - r >= 2) {
+            flag = false;
+            rightMin.push(leftMax.top());
+            leftMax.pop();
+            r++, l--;
+        }
+        return flag;
     }
-
-    void addNum(int num) 
-    {
-        if (lHeap.empty() || num <= *lHeap.rbegin()) 
-        {
-            lHeap.insert(num);
-        } 
-        else 
-        {
-            rHeap.insert(num);
+    void addNum(int num, int i) {
+        if (leftMax.empty() or num <= leftMax.top().first) {
+            leftMax.push({num, i});
+            l++;
+        } else {
+            rightMin.push({num, i});
+            r++;
         }
-
         balance();
     }
-
-    void remove(int num) 
-    {
-        if (num <= *lHeap.rbegin()) 
-        {
-            lHeap.erase(lHeap.find(num));
-        } 
-        else 
-        {
-            rHeap.erase(rHeap.find(num));
+    double findMedian(int i) {
+        while (!leftMax.empty() and leftMax.top().second <= i) {
+            leftMax.pop();
         }
-
+        while (!rightMin.empty() and rightMin.top().second <= i) {
+            rightMin.pop();
+        }
         balance();
-    }
 
-    double median() 
-    {
-        if (lHeap.size() == rHeap.size()) 
-        {
-            return (*lHeap.rbegin() + *rHeap.begin()) / 2.0;
-        } 
-        else 
-        {
-            return *lHeap.rbegin();
+        if (l == r)
+            return ((leftMax.top().first + rightMin.top().first) / 2.0);
+        else
+            return leftMax.top().first;
+    }
+    void fixInvalidate(int i, vector<int>& nums) {
+        // if element to be removed is at top of any heap then pop them
+        // else find out in which heap they are hiding and remove size of that heap
+        if (nums[i] == leftMax.top().first and leftMax.top().second == i) {
+            leftMax.pop();
+            l--;
+        } else if (nums[i] == rightMin.top().first and
+                   rightMin.top().second == i) {
+            rightMin.pop();
+            r--;
+        } else if (nums[i] <= leftMax.top().first) {
+            l--;
+        } else {
+            r--;
         }
     }
-
     vector<double> medianSlidingWindow(vector<int>& nums, int k) {
-        vector<double> res;
-
-        for (int i = 0; i < nums.size(); i++) 
-        {
-            addNum(nums[i]);
-
-            if (i >= k) 
-            {
-                remove(nums[i - k]);
-            }
-
-            if (i >= k - 1) 
-            {
-                res.push_back(median());
-            }
+        for (int i = 0; i < k; i++) {
+            addNum(nums[i], i);
         }
+        ans.push_back(findMedian(-1));
+        int i = 0, j = k, n = nums.size();
 
-        return res;
+        while (j < n) {
+            // find ith element or adjust it
+            fixInvalidate(i, nums);
+            addNum(nums[j], j);
+            j++;
+            // while finding meadian we check if top element is inavlid 
+            // if index of that element is less than equal to i (out of window)
+            ans.push_back(findMedian(i));
+            i++;
+        }
+        return ans;
     }
 };
