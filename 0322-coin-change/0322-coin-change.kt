18478e68720360184b8eb9b077f070153fc29ce9
Ｍｏｈａｎ　Ｄａｓｓ <133029595+dos-shaman@users.noreply.github.com>
2024-07-11 09:10:16 +0530

    Time: 160 ms (94.01%), Space: 36.4 MB (76.30%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun coinChange(coins: IntArray, amount: Int): Int {
+
+    val dp = IntArray(amount + 1) { amount + 1 }
+    dp[0] = 0
+
+    for (coin in coins) {
+        for (i in coin..amount) {
+            dp[i] = minOf(dp[i], dp[i - coin] + 1)
+        }
+    }
+    return if (dp[amount] > amount) -1 else dp[amount]
+    }
+}
